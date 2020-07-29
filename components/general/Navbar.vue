5f9da6821f636ae02ae68afaa67a4305cb6af350
Marcos Arreguín <54511809+marcosmap1998@users.noreply.github.com>
2020-07-29 17:19:37 -0500

    Add files via upload

--- components/general/Navbar.vue
+++ components/general/Navbar.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
-    <ul>
-      <li><nuxt-link to="/home">Home</nuxt-link></li>
-      <li><nuxt-link to="/chat">Chat</nuxt-link></li>
-      <li><nuxt-link to="/dudas">Dudas</nuxt-link></li>
-    </ul>
+    <b-navbar toggleable="lg" type="dark" variant="info" class="fixed-bottom">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav class="justify-content-center">
+        <b-navbar-nav>
+          <b-nav-item href="/home" class="mx-3 icono"
+            ><i class="fas fa-home"></i
+          ></b-nav-item>
+          <b-nav-item href="#" class="mx-3 icono"
+            ><i class="fas fa-phone"></i
+          ></b-nav-item>
+          <b-nav-item href="#" class="mx-3 icono"
+            ><i class="fas fa-map-marker-alt"></i
+          ></b-nav-item>
+          <b-nav-item href="#" class="mx-3 icono"
+            ><i class="fas fa-info-circle"></i
+          ></b-nav-item>
+          <b-nav-item href="#" class="mx-3 icono"
+            ><i class="fas fa-comment-alt"></i
+          ></b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
+
+<script>
+export default {
+  layoutTransition: {
+    name: 'layout',
+    mode: 'out-in',
+  },
+}
+</script>
+
+<style>
+.icono {
+  font-size: 2em;
+}
+</style>
