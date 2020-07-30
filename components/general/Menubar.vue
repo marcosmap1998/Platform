e2e338861ed952ae6cdca5be31616f14dd55359c
Marcos Arreguín <54511809+marcosmap1998@users.noreply.github.com>
2020-07-30 17:17:33 -0500

    Add files via upload

---
+++ components/general/Menubar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="general-container">
+    <b-container>
+      <b-row class="justify-content-center mt-4">
+        <b-col lg="8" class="d-flex flex-row justify-content-between">
+          <div>
+            <h3>a Salvo</h3>
+          </div>
+          <div class="regresar mt-1">
+            <a href=""><h5>Regresar</h5></a>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style>
+.regresar a {
+  color: #000;
+}
+
+.regresar a:hover {
+  color: #000;
+  text-decoration: none;
+}
+</style>
--- components/general/Navbar.vue
+++ components/general/Navbar.vue
@@ -1,28 +1,41 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="info" class="fixed-bottom">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav class="justify-content-center">
-        <b-navbar-nav>
-          <b-nav-item href="/home" class="mx-3 icono"
+  <b-navbar
+    toggleable="sm"
+    fixed="bottom"
+    class="barra_nav justify-content-end"
+  >
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav class="justify-content-center">
+      <b-navbar-nav>
+        <b-nav-item href="/home" class="mx-3 icono align-self-center">
+          <nuxt-link to="/home" class="link"
             ><i class="fas fa-home"></i
-          ></b-nav-item>
-          <b-nav-item href="#" class="mx-3 icono"
+          ></nuxt-link>
+        </b-nav-item>
+        <b-nav-item href="#" class="mx-3 icono align-self-center">
+          <nuxt-link to="/" class="link"
             ><i class="fas fa-phone"></i
-          ></b-nav-item>
-          <b-nav-item href="#" class="mx-3 icono"
+          ></nuxt-link>
+        </b-nav-item>
+        <b-nav-item href="#" class="mx-3 icono align-self-center">
+          <nuxt-link to="/mapa" class="link"
             ><i class="fas fa-map-marker-alt"></i
-          ></b-nav-item>
-          <b-nav-item href="#" class="mx-3 icono"
+          ></nuxt-link>
+        </b-nav-item>
+        <b-nav-item href="#" class="mx-3 icono align-self-center">
+          <nuxt-link to="/" class="link"
             ><i class="fas fa-info-circle"></i
-          ></b-nav-item>
-          <b-nav-item href="#" class="mx-3 icono"
+          ></nuxt-link>
+        </b-nav-item>
+        <b-nav-item href="#" class="mx-3 icono align-self-center">
+          <nuxt-link to="/" class="link"
             ><i class="fas fa-comment-alt"></i
-          ></b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+          ></nuxt-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -35,7 +48,26 @@ export default {
 </script>
 
 <style>
+.barra_nav {
+  height: 10vh;
+  background-color: #12cdd4;
+}
+
 .icono {
   font-size: 2em;
+}
+
+.link {
+  color: #fedfda;
+}
+
+.link:hover {
+  color: #fff;
+}
+
+@media screen and (max-width: 800px) {
+  .icono {
+    font-size: 1.5em;
+  }
 }
 </style>
