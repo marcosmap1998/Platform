3924edf462af8d2a0542964293e47c47a8fa3e43
Marcos Arreguín <54511809+marcosmap1998@users.noreply.github.com>
2020-07-31 13:45:11 -0500

    Add files via upload

---
+++ components/general/Menubar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menubar__container w-100 position-fixed">
+    <b-container>
+      <b-row class="justify-content-center mt-4">
+        <b-col lg="8" class="d-flex flex-row justify-content-between">
+          <div>
+            <h3>aSalvo</h3>
+          </div>
+          <div class="regresar mt-1" @click="logout()">
+            <h5>Salir</h5>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions('authStore', ['logout']),
+  },
+}
+</script>
+
+<style>
+.regresar a {
+  color: #000;
+}
+
+.regresar a:hover {
+  color: #000;
+  text-decoration: none;
+}
+</style>
--- components/general/Navbar.vue
+++ components/general/Navbar.vue
@@ -1,8 +1,63 @@
 <template>
   <div>
-    <ul>
-      <li><nuxt-link to="/">Home</nuxt-link></li>
-      <li><nuxt-link to="/chat">Chat</nuxt-link></li>
-    </ul>
+    <b-navbar
+      toggleable="lg"
+      type="dark"
+      variant="info"
+      class="navbar-inverse fixed-bottom"
+    >
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav class="justify-content-center">
+        <b-navbar-nav>
+          <div class="mx-3 icono">
+            <nuxt-link class="text-white" to="/home"
+              ><i class="fas fa-home"></i
+            ></nuxt-link>
+          </div>
+          <div class="mx-3 icono">
+            <nuxt-link class="text-white" to="/home"
+              ><i class="fas fa-map-marker-alt"></i
+            ></nuxt-link>
+          </div>
+          <div class="mx-3 icono">
+            <nuxt-link class="text-white" to="/home"
+              ><i class="fas fa-info-circle"></i
+            ></nuxt-link>
+          </div>
+          <div class="mx-3 icono">
+            <nuxt-link class="text-white" to="/salvi"
+              ><i class="fas fas fa-comment-alt"></i
+            ></nuxt-link>
+          </div>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
+
+<script>
+export default {
+  layoutTransition: {
+    name: 'layout',
+    mode: 'out-in',
+  },
+}
+</script>
+
+<style>
+.barra_nav {
+  background-color: #12cdd4;
+}
+
+.icono {
+  font-size: 2em;
+}
+
+.link {
+  color: #fedfda;
+}
+
+.link:hover {
+  color: #fff;
+}
+</style>
